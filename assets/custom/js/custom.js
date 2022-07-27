cc4a60273374d894364eee157cbd3df2acb77fe4
git-tolu <adejumobitoluwanimi2@gmail.com>
2022-07-27 16:44:08 +0100

    blog user login commit

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -26,17 +26,32 @@ $(document).ready(function () {
     //         $(".bor").hide();
     //     }
     // );
-  /**
-   * Animation on scroll
-   */
-   window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-  });
+    /**
+     * Animation on scroll
+     */
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
+
+    //   login and register animation
+    $('#loginLink').click(function () {
+        // $('form').animate({ height: "toggle", });
+        $('#register').show();
+    });
+    $('#registerLink').click(function () {
+        // $('form').animate({ height: "toggle", });
+        $('#login').hide();
+    });
+    // $('.link').click(function () {
+    //     $('form').animate({ height: "toggle", });
+    //   });
+
+
 });
 
 
