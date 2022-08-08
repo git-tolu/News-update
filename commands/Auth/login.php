125743f7fa2fecd675b27eb42d104f56d206394a
git-tolu <adejumobitoluwanimi2@gmail.com>
2022-08-08 15:20:04 +0100

    login api commit

--- commands/Auth/login.php
+++ commands/Auth/login.php
@@ -1,11 +1,67 @@
 <?php
 require "../models/apipost.php";
+require "../models/dbc.php";
+
+$dbc = new Dbc();
 
 if (($_SERVER['REQUEST_METHOD']) === 'POST') {
-    echo json_encode([
-        'message' => "successful",
-        'status' => 400
-    ]);
+    if ($dbc->validateInput($_POST['userEmail'])) {
+        $userEmail = $dbc->test_input($_POST['userEmail']);
+    } else {
+        http_response_code(403);
+        echo json_encode([
+            'emailerr' => 'email cannot be empty',
+            'status' => 403
+        ]);
+        die();
+    }
+
+    if ($dbc->validateInput($_POST['userPassword'])) {
+        $userPassword = $dbc->test_input($_POST['userPassword']);
+        if ($dbc->passwordRegex($userPassword)) {
+            http_response_code(403);
+            echo json_encode([
+                'passworderr' => $dbc->passwordRegex($userPassword),
+                'status' => 403
+            ]);
+            die();
+        }
+    } else {
+        http_response_code(403);
+        echo json_encode([
+            'passworderr' => 'password cannot be empty',
+            'status' => 403
+        ]);
+        die();
+    }
+
+    $login = $dbc->login($userEmail);
+
+    if ($login != null) {
+        if (password_verify($userPassword, $login['userPassword'])) {
+            http_response_code(200);
+            echo json_encode([
+                'login' => 'Successful',
+                'status' => 200
+            ]);
+            die();
+            $_SESSION['ourUser'] = $email;
+        } else {
+            http_response_code(400);
+            echo json_encode([
+                'loginerr' => 'invalid password',
+                'status' => 400
+            ]);
+            die();
+        }
+    } else {
+        http_response_code(400);
+        echo json_encode([
+            'loginerr' => 'User not found',
+            'status' => 400
+        ]);
+        die();
+    }
 } else {
     echo json_encode([
         'message' => "wrong method passed",
