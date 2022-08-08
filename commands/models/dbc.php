61808c46f848d3fe5557a33778f3fbeaeac83bae
git-tolu <adejumobitoluwanimi2@gmail.com>
2022-08-08 15:20:38 +0100

    dbc model commit

--- commands/models/dbc.php
+++ commands/models/dbc.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require "config.php";
 
 class Dbc extends Database
@@ -7,7 +7,7 @@ class Dbc extends Database
 
     public function validateInput($param)
     {
-        return (!empty($value));
+        return (!empty($param));
     }
 
     public function registerUser($userName, $userEmail, $userPassword)
@@ -19,5 +19,43 @@ class Dbc extends Database
             "userEmail" => $userEmail,
             "userPassword" => $userPassword
         ]);
+
+        return true;
+    }
+
+    public function emailCheck($email)
+    {
+        $sql = "SELECT userEmail FROM users WHERE userEmail = :email";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            "email" => $email
+        ]);
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function passwordRegex($password)
+    {
+        $uppercase = preg_match('@[A-Z]@', $password);
+        $lowercase = preg_match('@[a-z]@', $password);
+        $number    = preg_match('@[0-9]@', $password);
+        $specialChars = preg_match('@[^\w]@', $password);
+    
+        if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+            return "Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.";
+        }
+    }
+
+    public function login($email)
+    {
+        $sql = "SELECT userEmail, userPassword FROM users WHERE userEmail = :email";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            "email" => $email
+        ]);
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
     }
 }
