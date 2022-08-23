690eea36da43fbedba07bb1ba2db7900a84d5d4d
git-tolu <adejumobitoluwanimi2@gmail.com>
2022-08-23 13:12:47 +0100

    api changes

--- commands/Auth/login.php
+++ commands/Auth/login.php
@@ -8,7 +8,6 @@ if (($_SERVER['REQUEST_METHOD']) === 'POST') {
     if ($dbc->validateInput($_POST['userEmail'])) {
         $userEmail = $dbc->test_input($_POST['userEmail']);
     } else {
-        http_response_code(403);
         echo json_encode([
             'emailerr' => 'email cannot be empty',
             'status' => 403
@@ -19,15 +18,13 @@ if (($_SERVER['REQUEST_METHOD']) === 'POST') {
     if ($dbc->validateInput($_POST['userPassword'])) {
         $userPassword = $dbc->test_input($_POST['userPassword']);
         if ($dbc->passwordRegex($userPassword)) {
-            http_response_code(403);
-            echo json_encode([
+                echo json_encode([
                 'passworderr' => $dbc->passwordRegex($userPassword),
                 'status' => 403
             ]);
             die();
         }
     } else {
-        http_response_code(403);
         echo json_encode([
             'passworderr' => 'password cannot be empty',
             'status' => 403
@@ -39,15 +36,13 @@ if (($_SERVER['REQUEST_METHOD']) === 'POST') {
 
     if ($login != null) {
         if (password_verify($userPassword, $login['userPassword'])) {
-            http_response_code(200);
+            $_SESSION['ourUser'] = $userEmail;
             echo json_encode([
                 'login' => 'Successful',
                 'status' => 200
             ]);
             die();
-            $_SESSION['ourUser'] = $email;
         } else {
-            http_response_code(400);
             echo json_encode([
                 'loginerr' => 'invalid password',
                 'status' => 400
@@ -55,7 +50,6 @@ if (($_SERVER['REQUEST_METHOD']) === 'POST') {
             die();
         }
     } else {
-        http_response_code(400);
         echo json_encode([
             'loginerr' => 'User not found',
             'status' => 400
