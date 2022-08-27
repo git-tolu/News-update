8b75ade5c15685c4a8eb6542765715348d233003
git-tolu <adejumobitoluwanimi2@gmail.com>
2022-08-27 12:43:26 +0100

    am lost on names for my commit

---
+++ userDashboard/index.php
@@ -0,0 +1,119 @@
+<?php 
+    require "../commands/models/dbc.php";
+    if (isset($_SESSION['ourUser'])) {
+        header("location: dashboard");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login for user's dashboard</title>
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    
+    <!-- Custom css -->
+    <link rel="stylesheet" href="../assets/custom/css/style.css">
+</head>
+
+<body>
+
+    <div class="full-screen d-flex justify-content-center align-items-center text-center">
+        <div class="card p-2 box" style="max-width: 800px !important; height: auto  !important;">
+            <div class="row g-0 ">
+                <div class="col-md-6  d-none d-md-inline-block">
+                    <img src="../media/landing/—Pngtree—personal blog_2853664.png" class="card-img-top h-100" alt="...">
+                </div>
+                <div class="col-md-6 col-sm-12" >
+                    <div class="card-body">
+                        <div class="container">
+                            <!-- login form -->
+                            <form id="login" class="p-3">
+                                <div class="form-group mb-2">
+                                    <input type="email" class="form-control" name="userEmail" id="userName"
+                                        placeholder="Enter email" required="">
+                                        <span id="userEmailerr" class="text-danger"></span>
+                                </div>
+                                <div class="form-group mb-2">
+                                    <input type="password" class="form-control" name="userPassword" id="userEmail"
+                                        placeholder="Enter password" required="">
+                                        <span id="userPassworderr" class="text-danger"></span>
+                                </div>
+                                <div class="form-group mb-2">
+                                    <input type="button" class="form-control btn btn-outline-primary" name="inputSubmit"
+                                        id="loginBTN" value="Login">
+                                </div>
+                                <div class="form-group mb-2">
+                                    <div class="text-muted">Dont have an account ? <p id="loginLink" href=""
+                                            class="link text-primary">Register</p>
+                                    </div>
+                                </div>
+                            </form>
+                            <!-- register form -->
+                            <form id="register" style=" display: none;">
+                                <div class="form-group mb-2">
+                                    <input type="text" class="form-control" name="newUserName" id="newUserName"
+                                        placeholder="Enter your name">
+                                        <span id="newUserNameerr" class="text-danger"></span>
+                                </div>
+                                <div class="form-group mb-2">
+                                    <input type="email" class="form-control" name="newUserEmail" id="newUserEmail"
+                                        placeholder="Enter email">
+                                        <span id="newUserEmailerr" class="text-danger"></span>
+                                </div>
+                                <div class="form-group mb-2">
+                                    <input type="password" class="form-control" name="newUserPassword" id="newUserPassword"
+                                        placeholder="Enter password">
+                                        <span id="newUserPassworderr" class="text-danger"></span>
+                                </div>
+                                <div class="form-group mb-2">
+                                    <input type="password" class="form-control" name="newUserConfirmPassword" id="newUserConfirmPassword"
+                                        placeholder="Confirm passowrd">
+                                        <span id="newUserConfirmPassworderr" class="text-danger"></span>
+                                </div>
+                                <div class="form-group mb-2">
+                                    <input type="button" class="form-control btn btn-outline-primary" name="inputSubmit"
+                                        id="registerBTN" value="Register">
+                                </div>
+                                <div class="form-group mb-2">
+                                    <div class="text-muted">Already registered ? <p id="registerLink" href=""
+                                            class="link text-primary">login</p>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap js -->
+    <script src="../assets/bootstrap/js/jquery.min.js"></script>
+    <script src="../assets/bootstrap/js/popper.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/dist/js/bootstrap4.js"></script>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- JS for Owl carousel -->
+    <script src="../assets/plugins/owl.carousel/owl.carousel.min.js"></script>
+    <script src="../assets/plugins/owl.carousel/owl.custom.js"></script>
+    <!-- Transition js -->
+    <script src="../assets/plugins/aos/aos.js"></script>
+    <script src="../assets/plugins/glightbox/js/glightbox.min.js"></script>
+    <script src="../assets/plugins/swiper/swiper-bundle.min.js"></script>
+
+    <!-- sweet alert -->
+    <script src="../assets/bootstrap/js/sweet.js"></script>
+
+    <!--  Custom js -->
+    <script src="../assets/custom/js/custom.js"></script>
+    <script src="../assets/custom/js/main.js"></script>
+    <script src="../assets/custom/js/validate.js"></script>
+    <script src="../assets/custom/js/auth.js"></script>
+
+</body>
+
+</html>
