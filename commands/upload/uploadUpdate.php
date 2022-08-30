2d546c8b6e0751cff7e441889e13b0dacfe29b22
git-tolu <adejumobitoluwanimi2@gmail.com>
2022-08-30 13:59:40 +0100

    uploadUpdate commit

---
+++ commands/upload/uploadUpdate.php
@@ -0,0 +1,91 @@
+<?php
+require_once "../models/apipost.php";
+require_once "../models/dbc.php";
+
+$dbc = new Dbc();
+
+if (($_SERVER['REQUEST_METHOD']) === 'POST') {
+    if ($dbc->validateInput($_POST['updateId'])) {
+        $updateId = $dbc->test_input($_POST['updateId']);
+    } else {
+        echo json_encode([
+            'updateIderr' => 'updateId cannot be empty',
+            'status' => 403
+        ]);
+        die();
+    }
+    if ($dbc->validateInput($_POST['title'])) {
+        $title = $dbc->test_input($_POST['title']);
+    } else {
+        echo json_encode([
+            'titleerr' => 'title cannot be empty',
+            'status' => 403
+        ]);
+        die();
+    }
+
+    if ($dbc->validateInput($_POST['author'])) {
+        $author = $dbc->test_input($_POST['author']);
+    } else {
+        echo json_encode([
+            'authorerr' => 'author cannot be empty',
+            'status' => 403
+        ]);
+        die();
+    }
+
+    if ($dbc->validateInput($_POST['category'])) {
+        $category = $dbc->test_input($_POST['category']);
+    } else {
+        echo json_encode([
+            'categoryerr' => 'category cannot be empty',
+            'status' => 403
+        ]);
+        die();
+    }
+
+    if ($dbc->validateInput($_FILES['coverimage']['name'])) {
+        $coverimage = $dbc->test_input($_FILES['coverimage']['name']);
+    } else {
+        echo json_encode([
+            'coverimageerr' => 'coverimage cannot be empty',
+            'status' => 403
+        ]);
+        die();
+    }
+
+    if ($dbc->validateInput($_POST['content'])) {
+        $content = $dbc->test_input($_POST['content']);
+    } else {
+        echo json_encode([
+            'content' => 'textarea cannot be empty',
+            'status' => 403
+        ]);
+    }
+
+    if (!empty($title) && !empty($author) && !empty($category) && !empty($coverimage) && !empty($content)) {
+        $folder = '../uploadMedia/';
+        $newImage =  time() . '_' . rand(500000, 500000000000) . $coverimage;
+        move_uploaded_file($_FILES['coverimage']['tmp_name'], $folder . $newImage);
+        $result = $dbc->uploadUpdate($updateId, $title, $author, $category, $newImage, $content);
+        if ($result) {
+            // $_SESSION['ourUser'] = $newUserEmail;
+            echo json_encode([
+                'result' => 'Successfull',
+                'status' => 200
+            ]);
+            die();
+        } else {
+            echo json_encode([
+                'resulterr' => 'Sorry something went wrong',
+                'status' => 403
+            ]);
+            die();
+        }
+    }
+} else {
+    echo json_encode([
+        'message' => 'wrong method passed',
+        'status' => 400
+    ]);
+}
