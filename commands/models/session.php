7da1bf337b1b666b0b395e78926671998c8d3d26
git-tolu <adejumobitoluwanimi2@gmail.com>
2022-09-14 10:36:06 +0100

    session changes

--- commands/models/session.php
+++ commands/models/session.php
@@ -1,5 +1,12 @@
 <?php
     require_once "dbc.php";
+    $dbc= new Dbc();
     if (!isset($_SESSION['ourUser'])) {
         header("location: index");
     }
+
+    $cession = $dbc->emailCheck($_SESSION['ourUser']);
+    $cid = $cession['id'];
+    $cstatus = $cession['status'];
+    $cname = $cession['userName'];
+    // echo $cstatus;
